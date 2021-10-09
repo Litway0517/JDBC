c46e59d007201664d399b2bf73cd37895a941d30
Mingw0717-Dell <xmw0517@gmail.com>
2021-10-09 16:55:27 +0800

    Initial commit

---
+++ JDBC_1/src/com/atguigu/connection/ConnectionTest.java
@@ -0,0 +1,30 @@
+package com.atguigu.connection;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionTest {
+
+    @Test
+    public void testConnection1() throws SQLException {
+
+
+        Driver driver = new com.mysql.jdbc.Driver();
+
+        String url = "jdbc:mysql://localhost:3306/test";
+
+        // 将用户名和密码封装在Properties中
+        Properties info = new Properties();
+        info.setProperty("user", "root");
+        info.setProperty("password", "root");
+
+        Connection conn = driver.connect(url, info);
+        System.out.println(conn);
+
+
+    }
+}
